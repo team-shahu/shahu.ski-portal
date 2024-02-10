73a3811c04173a82a8c061993452b0e0e0f332e3
mai <74494945+chan-mai@users.noreply.github.com>
2024-02-10 18:07:20 +0000

    add: お知らせ追加

--- src/App.vue
+++ src/App.vue
@@ -43,5 +43,27 @@
       </div>
     </nav>
   </header>
-  <router-view></router-view>
+  
+  <div class="bg-gradient-to-r from-red-500 via-purple-400 to-blue-500">
+    <div class="max-w-[85rem] px-4 py-4 sm:px-6 lg:px-8 mx-auto">
+      <div class="grid justify-center md:grid-cols-2 md:justify-between md:items-center gap-2">
+        <div class="text-center md:text-start">
+          <p class="text-xs text-white/[.8] uppercase tracking-wider">
+            重要なお知らせ
+          </p>
+          <p class="mt-1 text-white font-medium">
+            スマートフォンでしゃふすきーをご利用の方へ
+          </p>
+        </div>
+
+        <div class="mt-3 text-center md:text-start md:flex md:justify-end md:items-center">
+          <a class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-full border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
+            Read more
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <router-view class="mt-5"></router-view>
 </template>
