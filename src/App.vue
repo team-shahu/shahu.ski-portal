14e5a8339a8993f14ae25461d329b828eef0ab52
mai <74494945+chan-mai@users.noreply.github.com>
2024-02-11 11:59:45 +0000

    fix: アナウンスの画面遷移

--- src/App.vue
+++ src/App.vue
@@ -57,9 +57,9 @@
         </div>
 
         <div class="mt-3 text-center md:text-start md:flex md:justify-end md:items-center">
-          <a class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-full border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
+          <router-link class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-full border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" to="/content/doc/ktlgo8h43jzc">
             Read more
-          </a>
+          </router-link>
         </div>
       </div>
     </div>
